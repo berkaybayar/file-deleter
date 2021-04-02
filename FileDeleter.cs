b2ef555c2001217eca529cbc7ee6ea88bb970738
berkaybayar <77205615+berkaybayar@users.noreply.github.com>
2021-04-02 21:34:15 +0300

    Update FileDeleter.cs

--- FileDeleter.cs
+++ FileDeleter.cs
@@ -15,11 +15,9 @@ namespace WindowsFormsAppText //ENTER YOUR PROJECT NAMESPACE HERE TO USE VARIABL
         public static string systemdir = Environment.GetFolderPath(Environment.SpecialFolder.System);//gets system32 directory C:\Windows\System32
         public static string logdir = currentdir + "\\logs\\"; //log file directory
         public static string path; //path of the file that will be deleted
-
         public static ArrayList faillog = new ArrayList();//to log deletion failed items
         public static ArrayList succlog = new ArrayList(); //to log deletion successful items
-
-
+        
         public static int fitemcount = 1; //failed deletion item count 
         public static int sitemcount = 1; //success deletion item count
 
@@ -31,7 +29,7 @@ namespace WindowsFormsAppText //ENTER YOUR PROJECT NAMESPACE HERE TO USE VARIABL
 
 
 
-    void deleteallfiles() //deletes all files in a folder or deletes the file 
+    public static void deleteallfiles() //deletes all files in a folder or deletes the file 
     {
         /*
         Before this action send the folder or the file you wanna delete to path string variable if you dont do this action will fail
@@ -106,7 +104,7 @@ namespace WindowsFormsAppText //ENTER YOUR PROJECT NAMESPACE HERE TO USE VARIABL
         }
     }
 
-    void successlogger() //logger for successful folders,files deletion
+        public static void successlogger() //logger for successful folders,files deletion
     {
         Directory.CreateDirectory(logdir);//creates log folder in current directory
         TextWriter sw = new StreamWriter(logdir + DateTime.Now.ToShortDateString() + " -succesful.txt", true); //creates and starts logging succesful deletions
@@ -119,7 +117,7 @@ namespace WindowsFormsAppText //ENTER YOUR PROJECT NAMESPACE HERE TO USE VARIABL
         clearsucclog();
     }
 
-    void faillogger() //logger for failed folders,files deletion
+        public static void faillogger() //logger for failed folders,files deletion
     {
         Directory.CreateDirectory(logdir);//creates log folder in current directory
         TextWriter sw = new StreamWriter(logdir + DateTime.Now.ToShortDateString() + " -failed.txt", true); //creates and starts logging failed deletions
@@ -132,20 +130,20 @@ namespace WindowsFormsAppText //ENTER YOUR PROJECT NAMESPACE HERE TO USE VARIABL
         clearfaillog();
     }
 
-    void clearfaillog()//clears failed deletion log array content
+        public static void clearfaillog()//clears failed deletion log array content
     {
 
             faillog.Clear();
         fitemcount = 1; //resets failed deletion item count 
     }
 
-    void clearsucclog()//clears successful deletion log array content
+        public static  void clearsucclog()//clears successful deletion log array content
     {
             succlog.Clear();
         sitemcount = 1; //resets success deletion item count
     }
 
-    void dellogfiles()//deletes log files and disables logging
+        public static void dellogfiles()//deletes log files and disables logging
     {
         //to prevent logging to log this action first disables the logging and after deletion of log file it enables again
         path = logdir;
@@ -154,13 +152,13 @@ namespace WindowsFormsAppText //ENTER YOUR PROJECT NAMESPACE HERE TO USE VARIABL
         enablelogging();
     }
 
-    void disablelogging()
+        public static void disablelogging()
     {
         flogenable = false;
         slogenable = false;
 
     }
-    void enablelogging()
+        public static void enablelogging()
     {
         flogenable = true;
         slogenable = true;
